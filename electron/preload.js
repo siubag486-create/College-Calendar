7671eb860b0e2451a7bdacfc56e3de16458a2daa
SWP <siubag486@gmail.com>
2026-03-13 10:26:31 +0900

    sweep

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("electronAPI", {
-    captureWallpaper: () => electron_1.ipcRenderer.invoke("capture-wallpaper"),
-    closeWindow: () => electron_1.ipcRenderer.send("close-window"),
-});
